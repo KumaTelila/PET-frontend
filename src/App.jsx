2cd137f0f5b5370b7291b16a9fb48aaf8dad7d1c
Kuma Telila <kumatelila26@gmail.com>
2024-07-24 16:28:00 +0300

    Content Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Dashboard from './components/Dashboard/Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AppHeader from "./components/appHeader/appHeader.jsx";
+import Login from "./components/auth/Login.jsx";
+import Register from "./components/auth/Register.jsx";
+import Sidebar from "./components/Dashboard/Sidebar/Sidebar.jsx";
+import Navbar from "./components/Dashboard/Navbar/Navbar.jsx";
+import { SidebarItems } from "./components/Dashboard/Sidebar/common/sidebarData.jsx";
+import TransactionTable from "./components/Dashboard/Transaction/TransactionTable.jsx";
 
 function App() {
+  const user = {
+    imageUrl: "dist/img/user2-160x160.jpg",
+    name: "Alexander Pierce",
+  };
+  const brand = {
+    imageUrl: "dist/img/AdminLTELogo.png",
+    name: "PET",
+  };
+  const isLoggedIn = true;
+
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+    <Router>
+      <div className="wrapper">
+        {isLoggedIn && <Navbar />}
+        {isLoggedIn && <Sidebar items={SidebarItems} user={user} brand={brand} />}
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<TransactionTable />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,62 +1,113 @@
-import React from 'react'
+import React, { useState } from "react";
+import api from "../../services/api";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [state, setState] = useState({
+    email: '',
+    password: '',
+  });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setState((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmitClick = async (e) => {
+    e.preventDefault();
+    const payload = {
+      email: state.email,
+      password: state.password,
+    };
+
+    try {
+      const response = await api.post('/login', payload);
+      if (response.status === 200) {
+        localStorage.setItem('token', response.data.token);
+        navigate('/dashboard');
+      }
+    } catch (error) {
+      console.error('Login error:', error.response.data);
+      alert('Login failed. Please try again.');
+    }
+  };
+
   return (
-    <body className="hold-transition login-page">
-    <div className="login-box">
-      {/* /.login-logo */}
-      <div className="card card-outline card-primary">
-        <div className="card-header text-center">
-          <a href="../../index2.html" className="h1">
-            <b>P</b>ET
-          </a>
+    <div className="login-page">
+      <div className="login-box">
+        <div className="card card-outline card-primary">
+          <div className="card-header text-center">
+            <a href="../../index2.html" className="h1">
+              <b>P</b>ET
+            </a>
+          </div>
+          <div className="card-body">
+            <p className="login-box-msg">Sign in to start your session</p>
+            <form onSubmit={handleSubmitClick}>
+              <div className="input-group mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                  id="email"
+                  value={state.email}
+                  onChange={handleChange}
+                />
+                <div className="input-group-append">
+                  <div className="input-group-text">
+                    <span className="fas fa-envelope"></span>
+                  </div>
+                </div>
+              </div>
+              <div className="input-group mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  id="password"
+                  value={state.password}
+                  onChange={handleChange}
+                />
+                <div className="input-group-append">
+                  <div className="input-group-text">
+                    <span className="fas fa-lock"></span>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-8">
+                  <div className="icheck-primary">
+                    <input
+                      type="checkbox"
+                      id="remember"
+                    />
+                    <label htmlFor="remember">
+                      Remember Me
+                    </label>
+                  </div>
+                </div>
+                <div className="col-4">
+                  <button type="submit" className="btn btn-primary btn-block">
+                    Sign In
+                  </button>
+                </div>
+              </div>
+            </form>
+            <p className="mb-1">
+              <a href="#">I forgot my password</a>
+            </p>
+            <p className="mb-0">
+              <a href="/register" className="text-center">Register a new membership</a>
+            </p>
+          </div>
         </div>
-        <div className="card-body">
-          <p className="login-box-msg">Sign in</p>
-
-          <form action="" method="post">
-            <div className="input-group mb-3">
-              <input type="email" className="form-control" placeholder="Email" />
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <span className="fas fa-envelope"></span>
-                </div>
-              </div>
-            </div>
-            <div className="input-group mb-3">
-              <input type="password" className="form-control" placeholder="Password" />
-              <div className="input-group-append">
-                <div className="input-group-text">
-                  <span className="fas fa-lock"></span>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-8">
-                <div className="icheck-primary">
-                  <input type="checkbox" id="remember" />
-                </div>
-              </div>
-              {/* /.col */}
-              <div className="col-4">
-                <button type="submit" className="btn btn-primary btn-block" onClick={() => (console.log('clicked'))}>Sign In</button>
-              </div>
-              {/* /.col */}
-            </div>
-          </form>
-          <p className="mb-1">
-            <a href="forgot-password.html">I forgot my password</a>
-          </p>
-          <p className="mb-0">
-            <a href="register.html" className="text-center">Register a new membership</a>
-          </p>
-        </div>
-        {/* /.card-body */}
       </div>
-      {/* /.card */}
     </div>
-    </body>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
