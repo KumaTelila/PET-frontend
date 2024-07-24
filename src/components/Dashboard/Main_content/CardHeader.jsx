f59b3dc1a884d7acec0cfc28c2eb45ef60936664
Kuma Telila <kumatelila26@gmail.com>
2024-07-24 20:33:22 +0300

    Modal template added

--- src/components/Dashboard/Main_content/CardHeader.jsx
+++ src/components/Dashboard/Main_content/CardHeader.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import Modal from "../../../Expenses/Modal";
 const CardHeader = ({ title }) => {
   return (
-    <div className="card-header">
-      <ul className="navbar nav">
-        <li className="nav-item">
-          <h3 className="card-title">{title}</h3>
-        </li>
-        <li className="nav-item">
-          <button className="btn btn-primary btn-sm m-1" href="#">
-            <i class="m-1 fas fa-plus"></i>
-            Add Expense
-          </button>
-        </li>
-      </ul>
-    </div>
+    <>
+      <Modal />
+      <div className="card-header">
+        <ul className="navbar nav">
+          <li className="nav-item">
+            <h3 className="card-title">{title}</h3>
+          </li>
+          <li className="nav-item">
+            <button
+              className="btn btn-primary btn-sm m-1"
+              type="button"
+              data-toggle="modal"
+              data-target="#modal-default"
+            >
+              <i class="m-1 fas fa-plus"></i>
+              Add Expense
+            </button>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
